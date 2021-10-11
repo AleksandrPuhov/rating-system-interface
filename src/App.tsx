1963aa4d24ef63a5080cfc924aad551e76cd8e34
Aleks Puxov <aleksandrpuhov123@gmail.com>
2021-10-11 20:11:56 +0300

    Создан шаблон через create-react-app. Добавлен Redux буду в нем хранить пользователей и их рейтинг. Так-же добавил Ant для ui. Для запросов позже еще axios добавлю.

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+import { Button } from "antd";
+import "antd/dist/antd.css";
+
+const App = () => {
+	return (
+		<div className="App">
+			<Button>Primary Button</Button>
+		</div>
+	);
+};
+
+export default App;
