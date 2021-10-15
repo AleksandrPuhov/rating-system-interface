34fd520a576eb2ed42aa9a2bcdb9b864e7673693
Aleks Puxov <aleksandrpuhov123@gmail.com>
2021-10-16 00:03:25 +0300

    Create gh-pages

--- src/store/axios-base.ts
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-	baseURL: "https://random-data-api.com/api/",
-});
-
-export default instance;
--- src/store/constants.ts
+++
@@ -1,4 +0,0 @@
-export enum TabNum {
-	Good = "1",
-	Bad = "2",
-}
